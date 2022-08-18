4d9d4d37ec32f325c4d0da4049e5f706efc4b796
Enzo <enzoefica@gmail.com>
2022-08-18 19:04:27 -0300

    Subiendo primer servicio

---
+++ Scripts/ScriptDatabase.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE tipbares;
+USE tipbares;
+
+insert into mesa(id, abierta, nro_mesa) values (1, true, 1);
+insert into mesa(id, abierta, nro_mesa) values (2, false, 2);
+commit;
+
+SELECT * FROM tipbares.mesa;
