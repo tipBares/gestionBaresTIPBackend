05eed86b21ba9eb4403856c96f19e7c55aa8bcb8
Enzo <enzoefica@gmail.com>
2022-08-24 20:27:16 -0300

    subiendo

--- gestionBares/src/main/java/com/tip/gestionBares/repositories/ProductoRepository.java
+++ gestionBares/src/main/java/com/tip/gestionBares/repositories/ProductoRepository.java
@@ -1,11 +1,13 @@
 package com.tip.gestionBares.repositories;
 
+
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tip.gestionBares.model.Producto;
+
 @Repository
 public interface ProductoRepository extends CrudRepository<Producto, UUID>{
 
