cf06769fd226b3b10ae87ff4b252feb6f2b8ef39
trinchero52 <trinchero52@gmail.com>
2022-08-25 20:28:42 -0300

    finalizado controller

--- gestionBares/src/main/java/com/tip/gestionBares/controller/ProductoController.java
+++ gestionBares/src/main/java/com/tip/gestionBares/controller/ProductoController.java
@@ -1,11 +1,17 @@
 package com.tip.gestionBares.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tip.gestionBares.dto.ProductoDto;
@@ -24,5 +30,13 @@ public class ProductoController {
 		
 		return new ResponseEntity<ProductoDto>(productoDto, HttpStatus.CREATED);
 		
+	}
+	@GetMapping
+	public ResponseEntity<List<ProductoDto>> findByName(@RequestParam String nombre) throws NotFoundException{
+		List<ProductoDto> productosDto = this.productoService.finByName(nombre);
+		if(productosDto.size() <= 0) {
+			throw new NotFoundException();
+		}
+		return new ResponseEntity<List<ProductoDto>>(productosDto, HttpStatus.OK);
 	}
 }
