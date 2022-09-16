2c279d764fed5f65484df6aa05a318eef81b64aa
trinchero52 <trinchero52@gmail.com>
2022-09-16 19:03:59 -0300

    resuelvo conflictos mozo

--- gestionBares/src/main/java/com/tip/gestionBares/controller/MozoController.java
+++ gestionBares/src/main/java/com/tip/gestionBares/controller/MozoController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.tip.gestionBares.dto.MozoDto;
@@ -23,7 +22,7 @@ import com.tip.gestionBares.service.MozoService;
 @CrossOrigin(origins = {"http://localhost:3000", "*"}, maxAge = 3600)
 public class MozoController {
 
-	@Autowired
+  @Autowired
 	private MozoService mozoService;
 
 	@GetMapping("/All")
